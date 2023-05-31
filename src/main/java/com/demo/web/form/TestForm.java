16a178121554346affc00fceef8859386d69fc9b
川内 大輝 <hiroki.kawauchi@satisjp.com>
2023-05-31 17:31:00 +0900

    first commit

---
+++ src/main/java/com/demo/web/form/TestForm.java
@@ -0,0 +1,22 @@
+package com.demo.web.form;
+
+public class TestForm {
+	private String faultDevice;
+	private String contractType;
+
+	public String getFaultDevice() {
+		return faultDevice;
+	}
+
+	public void setFaultDevice(String faultDevice) {
+		this.faultDevice = faultDevice;
+	}
+
+	public String getContractType() {
+		return contractType;
+	}
+
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+}
